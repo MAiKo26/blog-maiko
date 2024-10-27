8067d70c58feadb26afd1bd2977edec76c58b4c1
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-10-27 18:16:02 +0100

    Update LayoutWrapper.tsx

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -1,5 +1,5 @@
-import { Inter } from "next/font/google";
 import SectionContainer from "./SectionContainer";
+import { Inter } from "next/font/google";
 import Footer from "./Footer";
 import { ReactNode } from "react";
 import Header from "./Header";
